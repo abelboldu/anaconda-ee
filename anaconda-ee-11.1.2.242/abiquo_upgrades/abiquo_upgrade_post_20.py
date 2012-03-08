ed310e3d47fd7cdc7f9faf92ecdac79d5df4e40b
Abel Boldú <abel.boldu@gmx.com>
2012-03-08 11:03:34 +0100

    adding 2.0 upgrades

---
+++ anaconda-ee-11.1.2.242/abiquo_upgrades/abiquo_upgrade_post_20.py
@@ -0,0 +1,62 @@
+import os
+import iutil
+import shutil
+import logging
+import ConfigParser
+
+log = logging.getLogger("anaconda")
+
+def abiquo_upgrade_post(anaconda):
+
+    # apply the delta schema
+    schema_path = anaconda.rootPath + "/usr/share/doc/abiquo-server/database/kinton-delta-1_8_5-to-2_0_0.sql"
+    schema_path2 = anaconda.rootPath + "/usr/share/doc/abiquo-server/database/kinton-premium-delta-1.8.5-to-2.0.0.sql"
+
+    # Upgrade database if this is a server install
+    if os.path.exists(schema_path):
+        schema = open(schema_path)
+        log.info("ABIQUO: Updating Abiquo database (community delta)...")
+        iutil.execWithRedirect("/sbin/ifconfig",
+                                ['lo', 'up'],
+                                stdout="/dev/tty5", stderr="/dev/tty5",
+                                root=anaconda.rootPath)
+        
+        iutil.execWithRedirect("/etc/init.d/mysqld",
+                                ['start'],
+                                stdout="/mnt/sysimage/var/log/abiquo-postinst.log", stderr="//mnt/sysimage/var/log/abiquo-postinst.log",
+                                root=anaconda.rootPath)
+
+        iutil.execWithRedirect("/usr/bin/mysql",
+                                ['kinton'],
+                                stdin=schema,
+                                stdout="/mnt/sysimage/var/log/abiquo-postinst.log", stderr="//mnt/sysimage/var/log/abiquo-postinst.log",
+                                root=anaconda.rootPath)
+        schema.close()
+    
+    if os.path.exists(schema_path2):
+        schema = open(schema_path2)
+        log.info("ABIQUO: Updating Abiquo database (premium delta)...")
+        iutil.execWithRedirect("/sbin/ifconfig",
+                                ['lo', 'up'],
+                                stdout="/dev/tty5", stderr="/dev/tty5",
+                                root=anaconda.rootPath)
+        
+        iutil.execWithRedirect("/etc/init.d/mysqld",
+                                ['start'],
+                                stdout="/mnt/sysimage/var/log/abiquo-postinst.log", stderr="//mnt/sysimage/var/log/abiquo-postinst.log",
+                                root=anaconda.rootPath)
+
+        iutil.execWithRedirect("/usr/bin/mysql",
+                                ['kinton'],
+                                stdin=schema,
+                                stdout="/mnt/sysimage/var/log/abiquo-postinst.log", stderr="//mnt/sysimage/var/log/abiquo-postinst.log",
+                                root=anaconda.rootPath)
+        schema.close()
+
+    # restore fstab
+    backup_dir = anaconda.rootPath + '/opt/abiquo/backup/1.8.5'
+    if os.path.exists('%s/fstab.anaconda' % backup_dir):
+        shutil.copyfile("%s/fstab.anaconda" % backup_dir,
+                '%s/etc/fstab' % anaconda.rootPath)
+
+
