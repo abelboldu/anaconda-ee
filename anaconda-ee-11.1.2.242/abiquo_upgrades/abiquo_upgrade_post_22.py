f4079bc2bbeb9226c425b233fb713346fe9c29a4
Abel Boldú <abel.boldu@gmx.com>
2012-07-03 09:02:26 -0400

    new deltas

--- anaconda-ee-11.1.2.242/abiquo_upgrades/abiquo_upgrade_post_22.py
+++ anaconda-ee-11.1.2.242/abiquo_upgrades/abiquo_upgrade_post_22.py
@@ -8,10 +8,7 @@ log = logging.getLogger("anaconda")
 
 def abiquo_upgrade_post(anaconda):
 
-    # apply the delta schema
-    schema_path = anaconda.rootPath + "/usr/share/doc/abiquo-server/database/kinton-delta-1_8_5-to-2_0_0.sql"
-    schema_path2 = anaconda.rootPath + "/usr/share/doc/abiquo-server/database/kinton-premium-delta-1_8_5-to-2_0_0.sql"
-
+    schema_path = anaconda.rootPath + "/usr/share/doc/abiquo-server/database/kinton-latest-delta.sql"
     work_path = anaconda.rootPath + "/opt/abiquo/tomcat/work"
     temp_path = anaconda.rootPath + "/opt/abiquo/tomcat/temp"
     server_xml_path = anaconda.rootPath + "/opt/abiquo/tomcat/conf/Catalina/localhost/server.xml"
@@ -63,25 +60,6 @@ def abiquo_upgrade_post(anaconda):
                                 root=anaconda.rootPath)
         schema.close()
         
-    if os.path.exists(schema_path2):
-        schema = open(schema_path2)
-        log.info("ABIQUO: Updating Abiquo database (premium delta)...")
-        iutil.execWithRedirect("/sbin/ifconfig",
-                                ['lo', 'up'],
-                                stdout="/dev/tty5", stderr="/dev/tty5",
-                                root=anaconda.rootPath)
-        
-        iutil.execWithRedirect("/etc/init.d/mysqld",
-                                ['start'],
-                                stdout="/mnt/sysimage/var/log/abiquo-postinst.log", stderr="//mnt/sysimage/var/log/abiquo-postinst.log",
-                                root=anaconda.rootPath)
-
-        iutil.execWithRedirect("/usr/bin/mysql",
-                                ['kinton'],
-                                stdin=schema,
-                                stdout="/mnt/sysimage/var/log/abiquo-postinst.log", stderr="//mnt/sysimage/var/log/abiquo-postinst.log",
-                                root=anaconda.rootPath)
-        schema.close()
 
 
     # Redis patch on server
